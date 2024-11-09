00984edf5f915d5aea5a3cb84c95c375e9174b52
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-11-09 19:14:37 +0200

    some prettier changes

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,69 +1,55 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { Question } from "../models/question";
-import { useQuizBuilderStore } from "../store/useQuizBuilderStore";
-import { VALIDATION_ANSWER_MESSAGE } from "../constants";
+import { computed, ref } from 'vue'
+import { Question } from '../models/question'
+import { useQuizBuilderStore } from '../store/useQuizBuilderStore'
+import { VALIDATION_ANSWER_MESSAGE } from '../constants'
 
-const quizStore = useQuizBuilderStore();
+const quizStore = useQuizBuilderStore()
 
 const props = defineProps<{
-  question: Question;
-}>();
+  question: Question
+}>()
 
-const answerTitle = ref("");
-const validateAnswerTitle = ref("");
+const answerTitle = ref('')
+const validateAnswerTitle = ref('')
 
 const handleClickRemove = () => {
-  quizStore.removeQuestion(props.question.id);
-};
+  quizStore.removeQuestion(props.question.id)
+}
 
 const handleClickAddOption = () => {
-  if (answerTitle.value === "") {
-    validateAnswerTitle.value = VALIDATION_ANSWER_MESSAGE.empty;
-    return;
+  if (answerTitle.value === '') {
+    validateAnswerTitle.value = VALIDATION_ANSWER_MESSAGE.empty
+    return
   }
 
-  if (
-    props.question.options.find(
-      (option) =>
-        option.toLocaleLowerCase() === answerTitle.value.toLocaleLowerCase(),
-    )
-  ) {
-    validateAnswerTitle.value = VALIDATION_ANSWER_MESSAGE.duplicate;
-    return;
+  if (props.question.options.find((option) => option.toLocaleLowerCase() === answerTitle.value.toLocaleLowerCase())) {
+    validateAnswerTitle.value = VALIDATION_ANSWER_MESSAGE.duplicate
+    return
   }
 
-  quizStore.addOption(props.question.id, answerTitle.value);
-  answerTitle.value = "";
-  validateAnswerTitle.value = "";
-};
+  quizStore.addOption(props.question.id, answerTitle.value)
+  answerTitle.value = ''
+  validateAnswerTitle.value = ''
+}
 const handleClickRemoveOption = (option: string) => {
-  quizStore.removeOption(props.question.id, option);
-};
+  quizStore.removeOption(props.question.id, option)
+}
 
 const isRadioInputType = computed(() => {
-  return props.question.type === "single" || props.question.type === "boolean";
-});
+  return props.question.type === 'single' || props.question.type === 'boolean'
+})
 </script>
 
 <template>
   <v-card>
     <template #title>
-      <v-card-title>{{
-        props.question.title || "Введіть питання"
-      }}</v-card-title>
+      <v-card-title>{{ props.question.title || 'Введіть питання' }}</v-card-title>
     </template>
     <template #text>
       <div class="d-flex ga-5">
-        <v-text-field
-          max-width="350px"
-          label="Питання"
-          variant="outlined"
-          v-model.trim="props.question.title"
-        />
-        <v-btn size="x-large" color="red" @click="handleClickRemove"
-          >Видалити</v-btn
-        >
+        <v-text-field max-width="350px" label="Питання" variant="outlined" v-model.trim="props.question.title" />
+        <v-btn size="x-large" color="red" @click="handleClickRemove">Видалити</v-btn>
       </div>
       <div v-if="props.question.type !== 'boolean'" class="d-flex ga-5">
         <v-text-field
@@ -73,19 +59,10 @@ const isRadioInputType = computed(() => {
           v-model.trim="answerTitle"
           :error-messages="validateAnswerTitle"
         />
-        <v-btn size="x-large" color="green" @click="handleClickAddOption"
-          >Додати відповідь</v-btn
-        >
+        <v-btn size="x-large" color="green" @click="handleClickAddOption">Додати відповідь</v-btn>
       </div>
-      <v-radio-group
-        v-if="isRadioInputType"
-        v-model="props.question.correctAnswer"
-      >
-        <div
-          class="d-flex"
-          v-for="option of props.question.options"
-          :key="option"
-        >
+      <v-radio-group v-if="isRadioInputType" v-model="props.question.correctAnswer">
+        <div class="d-flex" v-for="option of props.question.options" :key="option">
           <v-radio
             class="flex-0-0 mr-2"
             :value="option"
@@ -102,11 +79,7 @@ const isRadioInputType = computed(() => {
         </div>
       </v-radio-group>
       <div v-else>
-        <div
-          class="d-flex"
-          v-for="option of props.question.options"
-          :key="option"
-        >
+        <div class="d-flex" v-for="option of props.question.options" :key="option">
           <v-checkbox
             class="flex-0-0 mr-2"
             :value="option"
@@ -115,11 +88,7 @@ const isRadioInputType = computed(() => {
             :checked="props.question.correctAnswer.includes(option)"
             v-model="props.question.correctAnswer"
           />
-          <v-btn
-            icon="$delete"
-            variant="plain"
-            @click="handleClickRemoveOption(option)"
-          />
+          <v-btn icon="$delete" variant="plain" @click="handleClickRemoveOption(option)" />
         </div>
       </div>
     </template>
--- src/pages/QuizBuilder.vue
+++ src/pages/QuizBuilder.vue
@@ -1,61 +1,56 @@
 <script setup lang="ts">
-import { useQuizBuilderStore } from "../store/useQuizBuilderStore";
-import { QuestionTypeList, QuestionTypes } from "../models/question";
-import Question from "../components/Question.vue";
-import { uuid } from "vue-uuid";
+import { useQuizBuilderStore } from '../store/useQuizBuilderStore'
+import { QuestionTypeList, QuestionTypes } from '../models/question'
+import Question from '../components/Question.vue'
+import { uuid } from 'vue-uuid'
 const questionTypesList: QuestionTypeList[] = [
   {
     id: 1,
-    title: "Правда/не правда",
-    type: "boolean",
+    title: 'Правда/не правда',
+    type: 'boolean',
   },
   {
     id: 2,
-    title: "З однією відповіддю",
-    type: "single",
+    title: 'З однією відповіддю',
+    type: 'single',
   },
   {
     id: 3,
-    title: "З декількома відповідями",
-    type: "multiple",
+    title: 'З декількома відповідями',
+    type: 'multiple',
   },
-];
+]
 
-const quizStore = useQuizBuilderStore();
+const quizStore = useQuizBuilderStore()
 
 const handleClickAddQuestion = (type: QuestionTypes) => {
-  quizStore.addQuestion(type);
-};
+  quizStore.addQuestion(type)
+}
 
 const handleClickSaveQuiz = () => {
   const quiz = {
     id: uuid.v4(),
     title: quizStore.quizTitle,
     questions: quizStore.questionList,
-  };
+  }
 
-  const localStorageQuizzes = localStorage.getItem("quiz");
+  const localStorageQuizzes = localStorage.getItem('quiz')
 
   if (!localStorageQuizzes) {
-    localStorage.setItem("quiz", JSON.stringify([quiz]));
+    localStorage.setItem('quiz', JSON.stringify([quiz]))
   } else {
-    const quizzes = JSON.parse(localStorageQuizzes);
-    quizzes.push(quiz);
-    localStorage.setItem("quiz", JSON.stringify(quizzes));
+    const quizzes = JSON.parse(localStorageQuizzes)
+    quizzes.push(quiz)
+    localStorage.setItem('quiz', JSON.stringify(quizzes))
   }
-};
+}
 </script>
 
 <template>
   <v-main class="px-10 pb-10">
     <h1 class="text-center">Конструктор</h1>
     <div class="d-flex ga-5 justify-start mt-5">
-      <v-text-field
-        max-width="350px"
-        label="Назва вікторини"
-        variant="outlined"
-        v-model="quizStore.quizTitle"
-      />
+      <v-text-field max-width="350px" label="Назва вікторини" variant="outlined" v-model="quizStore.quizTitle" />
       <v-menu>
         <template v-slot:activator="{ props }">
           <v-btn size="x-large" color="primary" :="props">Додати питання</v-btn>
@@ -71,16 +66,10 @@ const handleClickSaveQuiz = () => {
           </v-list-item>
         </v-list>
       </v-menu>
-      <v-btn size="x-large" color="primary" @click="handleClickSaveQuiz"
-        >Зберегти</v-btn
-      >
+      <v-btn size="x-large" color="primary" @click="handleClickSaveQuiz">Зберегти</v-btn>
     </div>
     <div class="d-flex flex-column ga-5">
-      <Question
-        v-for="question of quizStore.questionList"
-        :question
-        :key="question.id"
-      />
+      <Question v-for="question of quizStore.questionList" :question :key="question.id" />
     </div>
   </v-main>
 </template>
