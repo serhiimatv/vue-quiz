3a553102bd8e879fcdb53f7f87660664cd9d2f08
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-12-18 22:39:20 +0200

    add show quiz

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,17 +1,73 @@
 <script setup lang="ts">
+import { router } from '../router'
 import useQuizStore from '../store/useQuizStore'
 
 const quizStore = useQuizStore()
 </script>
 
 <template>
-  <v-sheet :elevation="21" width="50%" class="pepper rounded-lg">
+  <v-sheet v-if="!quizStore.isQuizFinished" :elevation="21" width="50%" class="pepper rounded-lg">
     <v-row>
       <v-col class="header"> {{ quizStore.currentQuestionCount }} з {{ quizStore.totalCountQuestion }} </v-col>
     </v-row>
     <v-row>
       <v-col>
         <h2>{{ quizStore.currentQuestion?.title }}</h2>
+      </v-col>
+    </v-row>
+    <!-- boolean and single Question -->
+    <v-row v-if="quizStore.currentQuestion?.type === 'boolean' || quizStore.currentQuestion?.type === 'single'">
+      <v-col>
+        <v-radio-group v-model="quizStore.activeQuestionChooseOption">
+          <v-radio v-for="option in quizStore.currentQuestion?.options" :key="option" :label="option" :value="option" />
+        </v-radio-group>
+      </v-col>
+    </v-row>
+    <!-- multiple Question -->
+    <v-row v-else>
+      <v-col>
+        <v-checkbox
+          v-for="option in quizStore.currentQuestion?.options"
+          v-model="quizStore.activeQuestionChooseOption"
+          :key="option"
+          :label="option"
+          :value="option"
+        />
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-end">
+      <v-col cols="auto">
+        <v-btn
+          v-if="quizStore.currentQuestionCount !== quizStore.totalCountQuestion"
+          color="primary"
+          :disabled="quizStore.activeQuestionChooseOption.length === 0"
+          @click="quizStore.nextQuestion"
+        >
+          Наступне питання
+        </v-btn>
+        <v-btn
+          v-else
+          color="primary"
+          :disabled="quizStore.activeQuestionChooseOption.length === 0"
+          @click="quizStore.nextQuestion"
+        >
+          Закінчити вікторину
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-sheet>
+  <v-sheet v-else :elevation="21" width="50%" class="pepper rounded-lg">
+    <v-row>
+      <v-col>
+        <h2>Вікторина завершена</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col> Ваш результат: {{ quizStore.score }} з {{ quizStore.totalCountQuestion }} </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary" @click="router.push('/')"> На головну</v-btn>
       </v-col>
     </v-row>
   </v-sheet>
--- src/store/useQuizStore.ts
+++ src/store/useQuizStore.ts
@@ -8,6 +8,11 @@ const useQuizStore = defineStore('quiz', () => {
 
   const quiz = ref<Quiz | null>(null)
   const questionIndex = ref(0)
+  const activeQuestionChooseOption = ref([])
+
+  const score = ref(0)
+
+  const isQuizFinished = ref(false)
 
   const currentQuestionCount = computed(() => {
     return questionIndex.value + 1
@@ -27,12 +32,45 @@ const useQuizStore = defineStore('quiz', () => {
     quiz.value = quizzesStore.quizzes.find((quiz) => quiz.id === id) || ({} as Quiz)
   }
 
+  const checkAnswerAndAddScore = () => {
+    if (currentQuestion.value?.type === 'boolean' || currentQuestion.value?.type === 'single') {
+      if (currentQuestion.value?.correctAnswer === activeQuestionChooseOption.value) {
+        score.value = score.value + 1
+      }
+    } else if (currentQuestion.value?.type === 'multiple') {
+      const optionPrice = 1 / currentQuestion.value?.correctAnswer.length
+
+      if (activeQuestionChooseOption.value.length > currentQuestion.value?.correctAnswer.length) return
+
+      activeQuestionChooseOption.value.forEach((option) => {
+        if (currentQuestion.value?.correctAnswer.includes(option)) {
+          score.value = score.value + optionPrice
+        }
+      })
+    }
+  }
+
+  const nextQuestion = () => {
+    checkAnswerAndAddScore()
+    if (quiz.value?.questions[questionIndex.value + 1]) {
+      questionIndex.value++
+    } else {
+      isQuizFinished.value = true
+    }
+
+    activeQuestionChooseOption.value = []
+  }
+
   return {
     quiz,
     setQuiz,
     currentQuestionCount,
     totalCountQuestion,
     currentQuestion,
+    activeQuestionChooseOption,
+    nextQuestion,
+    score,
+    isQuizFinished,
   }
 })
 
